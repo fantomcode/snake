48ac175542af38c789bc9ac5f08a5f91e80c34a7
fantomcode <fantom2307@gmail.com>
2020-09-16 14:52:54 +0300

    lesson2

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -10,8 +10,21 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world");
+            int x1 = 1;
+            int y1 = 3;
+            char symbl = '*';
+            Draw(x1, y1, symbl);
+
+            int x2 = 4;
+            int y2 = 5;
+            char symbl1 = '#';
+            Draw(x2, y2, symbl1);
             Console.ReadLine();
+        }
+        static void Draw(int x, int y, char symbl)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(symbl);
         }
     }
 }
