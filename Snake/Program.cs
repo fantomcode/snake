bcd0b2af4d372196b0103fca155d8733376a2799
fantomcode <fantom2307@gmail.com>
2020-09-21 12:24:37 +0300

    3 урок

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -10,21 +10,18 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            int x1 = 1;
-            int y1 = 3;
-            char symbl = '*';
-            Draw(x1, y1, symbl);
+            Point p1 = new Point();
+            p1.x = 1;
+            p1.y = 3;
+            p1.sym = '#';
+            p1.Draw();
 
-            int x2 = 4;
-            int y2 = 5;
-            char symbl1 = '#';
-            Draw(x2, y2, symbl1);
-            Console.ReadLine();
-        }
-        static void Draw(int x, int y, char symbl)
-        {
-            Console.SetCursorPosition(x, y);
-            Console.Write(symbl);
-        }
+            Point p2 = new Point();
+            p2.x = 4;
+            p2.y = 5;
+            p2.sym = '#';
+            p2.Draw();
+            Console.ReadLine();          
+        }        
     }
 }
